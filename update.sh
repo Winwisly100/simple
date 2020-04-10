93737ddea96a8aa888c33dcb137664af0c59e7fc
winwisely100 <60738732+Winwisly100@users.noreply.github.com>
2020-04-10 20:27:02 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i '$ d' go/packaging/linux-deb/usr/share/applications/simple.desktop
 echo "Exec=/usr/lib/simple/simple %u" >> go/packaging/linux-deb/usr/share/applications/simple.desktop
-echo "MimeType=x-scheme-handler/simple;" >> go/packaging/linux-deb/usr/share/applications/simple.desktop
+echo "MimeType=x-scheme-handler/gcn;" >> go/packaging/linux-deb/usr/share/applications/simple.desktop
